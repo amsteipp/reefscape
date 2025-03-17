c89740bd28dbf6e03371cc4a01017c874df421e0
maxxie543 <mstehura89@gmail.com>
2025-03-17 12:47:46 -0400

    Updated Tank Drive

--- max/tankdrive/Robot.java
+++ max/tankdrive/Robot.java
@@ -4,46 +4,174 @@
 
 package frc.robot;
 
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.Faults;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkBaseConfig;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
 import edu.wpi.first.util.sendable.SendableRegistry;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+
+import edu.wpi.first.wpilibj.Timer;
+
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
- * This is a demo program showing the use of the DifferentialDrive class, specifically it contains
+ * This is a demo program showing the use of the DifferentialDrive class,
+ * specifically it contains
  * the code necessary to operate a robot with tank drive.
  */
 public class Robot extends TimedRobot {
+
   private final DifferentialDrive m_robotDrive;
   private final Joystick m_leftStick;
   private final Joystick m_rightStick;
+  private final int left_deviceID = 1;
+  private final int right_deviceID = 2;
+  private final SparkMax m_leftDrive;
+  private final SparkMax m_rightDrive;
+  private final XboxController m_controller;
 
-  private final PWMSparkMax m_leftMotor = new PWMSparkMax(0);
-  private final PWMSparkMax m_rightMotor = new PWMSparkMax(1);
+  public final Timer m_timer;
+
+  // private final SparkMax m_leftMotor = new SparkMax(0);
+  // private final SparkMax m_rightMotor = new SparkMax(1);
 
   /** Called once at the beginning of the robot program. */
   public Robot() {
     // We need to invert one side of the drivetrain so that positive voltages
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.
-    // m_rightMotor.setInverted(true);
+    // m_rightMotor.setInverted(true); no need for this
 
     // m_robotDrive = new DifferentialDrive(m_leftMotor::set, m_rightMotor::set);
+    m_leftDrive = new SparkMax(left_deviceID, MotorType.kBrushed);
+    configure_and_invert_motor(m_leftDrive);
+
+    m_rightDrive = new SparkMax(right_deviceID, MotorType.kBrushed);
+    configure_motor(m_rightDrive);
+
+    m_robotDrive = new DifferentialDrive(m_leftDrive::set, m_rightDrive::set);
+    m_controller = new XboxController(0);
+    m_timer = new Timer();
 
     m_leftStick = new Joystick(0);
     m_rightStick = new Joystick(1);
 
+    SendableRegistry.addChild(m_robotDrive, m_leftDrive);
+    SendableRegistry.addChild(m_robotDrive, m_rightDrive);
+  }
 
+  private void configure_motor(SparkMax motor) {
+    // Create a configuration object to set the properties for the left motor
+    SparkMaxConfig motor_config = new SparkMaxConfig();
 
+    // Configure motor settings
+    // NOTE: innverting direction of a speed controller if pass true to
+    // inverted(true)
+    // NOTE: This call has no effect if the controller is a follower.
+    // NOTE: To invert a follower, see the follow() method.
+    motor_config.inverted(false) // Set to true if you need to invert the motor
+        .idleMode(SparkBaseConfig.IdleMode.kBrake) // Use brake mode
+        .smartCurrentLimit(40) // Set current limit to 40 amps
+        .openLoopRampRate(0.5); // Ramp rate of 0.5 seconds from 0 to full
 
+    // Apply the configuration to the SPARK MAX
+    motor.configure(
+        motor_config,
+        ResetMode.kNoResetSafeParameters, // Don't reset existing parameters
+        PersistMode.kNoPersistParameters // Don't save to flash memory
 
-    SendableRegistry.addChild(m_robotDrive, m_leftMotor);
-    SendableRegistry.addChild(m_robotDrive, m_rightMotor);
+        /*
+        * see configure_and_invert method for comments
+        * on ResetMode and PersistMode
+        */
+    );
+  }
+
+  private void configure_and_invert_motor(SparkMax motor) {
+    // Create a configuration object to set the properties for the left motor
+    SparkMaxConfig motor_config = new SparkMaxConfig();
+
+    // Configure motor settings
+    // NOTE: innverting direction of a speed controller if pass true to
+    // inverted(true)
+    // NOTE: This call has no effect if the controller is a follower.
+    // NOTE: To invert a follower, see the follow() method.
+    motor_config.inverted(true) // Set to true if you need to invert the motor
+        .idleMode(SparkBaseConfig.IdleMode.kBrake) // Use brake mode
+        .smartCurrentLimit(40) // Set current limit to 40 amps
+        .openLoopRampRate(0.5); // Ramp rate of 0.5 seconds from 0 to full
+
+    // Apply the configuration to the SPARK MAX
+    motor.configure(
+        motor_config,
+        ResetMode.kNoResetSafeParameters, // Don't reset existing parameters
+        PersistMode.kNoPersistParameters // Don't save to flash memory
+
+    /*
+     * Understanding SparkBase.PersistMode.kNoPersistParameters
+     * In the REVLib 2025 API, SparkBase.PersistMode.kNoPersistParameters
+     * is used to control whether configuration settings are saved to the
+     * SPARK MAX controller's non-volatile memory (flash memory).
+     * 
+     * When you use PersistMode.kNoPersistParameters (which appears to be
+     * the updated name for what was previously called kDoNotPersist),
+     * the configuration changes you make:
+     * 
+     * Are applied immediately to the controller's active settings
+     * Are NOT saved to flash memory
+     * Will be lost when power is cycled to the controller
+     * 
+     * When to Use kNoPersistParameters:
+     * During development and testing:
+     * When you're frequently changing settings
+     * When you're testing different configurations
+     * When you want to avoid excessive writes to flash memory
+     * For runtime adjustments:
+     * Settings that might change during operation
+     * Temporary configurations
+     * For safety-critical parameters:
+     * When you want to ensure the robot always starts with known default values
+     */
+
+    );
+
   }
 
   @Override
   public void teleopPeriodic() {
     m_robotDrive.tankDrive(-m_leftStick.getY(), -m_rightStick.getY());
+    if (m_controller.getAButton()) {
+      // do what we want when A is pressed
+    }
   }
+
+  
+
+  @Override
+  public void autonomousInit() {
+    // Code that runs once when entering autonomous mode
+
+    m_timer.restart();
+
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+    // Drive for 2 seconds
+    if (m_timer.get() < 2.0) {
+      // Drive forwards half speed, make sure to turn input squaring off
+      m_robotDrive.arcadeDrive(0.5, 0.0, false);
+    } else {
+      m_robotDrive.stopMotor(); // stop robot
+    }
+  }
+  
 }
